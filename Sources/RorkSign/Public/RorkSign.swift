63491fad4557f1ffe8fa357f64d54a3f95d7ad65
Evgenii Mozharovskii <mozharovsky@live.com>
2026-06-17 20:45:52 +0100

    chore: release 0.2.22

--- Sources/RorkSign/Public/RorkSign.swift
+++ Sources/RorkSign/Public/RorkSign.swift
@@ -1801,7 +1801,7 @@ public struct HostedBundleSigningOptions: Equatable {
 public enum RorkSigner {
     /// Package version for CLI diagnostics and consumers that expose signer info.
     public static var version: String {
-        "0.2.21"
+        "0.2.22"
     }
 
     /// Reads high-level Mach-O metadata needed by signing and diagnostics.
